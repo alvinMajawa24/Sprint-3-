df6de3667c2de97855477062fa24ed15fe05aa7b
Alfred76-web <alfred.njaku@strathmore.edu>
2024-05-21 08:48:51 +0300

    #edit

--- in.php
+++ in.php
@@ -1,3 +1,21 @@
 <?php
-    print "This is my first code";
+    echo'This is my first code';
+    print '<br>'; // using the html break line tag
+
+    print "Today is <span style = 'color: #ff0040;'>Tuesday
+    </span>";//using css in a sentence
+
+    print '<br>'; // using the html break line tag
+
+    print 160819;
+
+    print '<br>'; // using the html break line tag
+
+    print date('1');
+
+    print '<br>'; // using the html break line tag
+
+    print date('Y');
+
+
     ?>
