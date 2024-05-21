f268f3c59b87f86f2e291879d68e481fa38787a6
Alfred76-web <alfred.njaku@strathmore.edu>
2024-05-21 09:25:19 +0300

    #calculating age

--- in.php
+++ in.php
@@ -26,12 +26,34 @@
 
     print '<br>'; // using the html break line tag
 
-    print date ('1,js-F-Y',strtotime('+3days'));
+    print date ('1,js-F-Y',strtotime('+3 months'));
 
     print '<br>'; // using the html break line tag
 
     print date('H:i:s:u');
-    
+
+    print '<br>'; // using the html break line tag
+
+    //create (Declare) a PHP variable
+    $fname = " Alex Mwaniki";
+    print $fname;
+
+    print '<br>'; // using the html break line tag
+    $yob = "2004";
+    print $yob;
+
+    print '<br>'; // using the html break line tag
+    $current_year = date('Y');
+    print $current_year;
+
+    print '<br>'; // using the html break line tag
+
+    $age = $current_year-$yob;
+    print $age;
+
+
+
+
 
 
 
